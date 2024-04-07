ccae6326934f11e674830d53c9d97ce85e0a723c
twentyone37 <twentyone_37@proton.me>
2024-04-07 15:38:29 +0200

    Update converters

--- src/app.ts
+++ src/app.ts
@@ -23,12 +23,12 @@ app.use(
 // POST endpoint for ARS to USDC conversion
 app.post("/convert-ars-to-usdc", async (req, res) => {
   try {
-    const { arsAmount } = req.body;
-    if (!arsAmount) {
+    const { amount } = req.body;
+    if (!amount) {
       return res.status(400).send({ error: "ARS amount is required" });
     }
-    const usdcValue = await convertARStoUSDC(arsAmount);
-    return res.json({ arsAmount, usdcValue });
+    const usdcValue = await convertARStoUSDC(amount);
+    return res.json({ arsValue: amount, usdcValue });
   } catch (error) {
     console.error(error);
     return res.status(500).send({ error: "Error converting currency" });
@@ -38,12 +38,12 @@ app.post("/convert-ars-to-usdc", async (req, res) => {
 // POST endpoint for USDC to ARS conversion
 app.post("/convert-usdc-to-ars", async (req, res) => {
   try {
-    const { usdcAmount } = req.body;
-    if (!usdcAmount) {
+    const { amount } = req.body;
+    if (!amount) {
       return res.status(400).send({ error: "USDC amount is required" });
     }
-    const arsValue = await convertUSDCToARS(usdcAmount);
-    return res.json({ usdcAmount, arsValue });
+    const arsValue = await convertUSDCToARS(amount);
+    return res.json({ usdcValue: amount, arsValue });
   } catch (error) {
     console.error(error);
     return res.status(500).send({ error: "Error converting currency" });
