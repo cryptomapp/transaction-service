d4718a97b80020768bd16caba5e193ba4edc0206
twentyone37 <twentyone_37@proton.me>
2024-05-23 09:44:12 +0200

    Update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,16 +1,25 @@
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
 import dotenv from "dotenv";
 dotenv.config();
+
+function createBase58Keypair(
+  secretArrayStr: string | undefined
+): string | undefined {
+  if (!secretArrayStr) return undefined;
+  const secretArray = JSON.parse(secretArrayStr);
+  const secretUint8Array = Uint8Array.from(secretArray);
+  const keypair = Keypair.fromSecretKey(secretUint8Array);
+  return bs58.encode(keypair.secretKey);
+}
 
 export const config = {
   cryptoMappProgramId:
     process.env.PROGRAM_ADDRESS ||
     "8mDhNcko1rByfWLzVTuddx386JFwFnD3oDPWV2pzBckN",
   solanaProviderUrl: process.env.SOLANA_RPC || "https://api.devnet.solana.com",
-  solPrivateKey:
-    process.env.SERVICE_WALLET ||
-    "4GVrk3J7GB8a29RqqBG6x9WZieDufp3ngPc6zcRH3JzfQN3WxLjMbgf98feTWdivSoXxM5EqpQQtzZzrAG6qJDMs",
-  stateAddress:
-    process.env.STATE_ADDRESS || "5HzkGM1XFoVrrPLjanQ7Le1Aa4iHPf3aivfKLUztmwFn",
+  solPrivateKey: createBase58Keypair(process.env.SERVICE_WALLET_SECRET_KEY),
+
   port: process.env.PORT || 8080,
   timeout: 60000, // 60s
 };
